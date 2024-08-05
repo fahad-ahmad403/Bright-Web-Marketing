1123f8886a98cff638f6149b38d7edf388681299
Fahad Ahmad <fahadahmad48224@gmail.com>
2024-08-05 21:08:17 +0500

    file

---
+++ script.js
@@ -0,0 +1,7 @@
+function open_navbar() {
+    document.getElementById("mobile_display").style.display = "flex";
+}
+
+function close_navbar() {
+    document.getElementById("mobile_display").style.display = "none";
+}
